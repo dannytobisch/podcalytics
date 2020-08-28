9dd1b5151b2870ad06dfc33f7a284d04f82c85b8
Danny Tobisch <danny.tobisch@gmail.com>
2020-08-28 16:34:53 +0200

    hardcoded url and imgs

--- awesome_streamlit/__init__.py
+++ awesome_streamlit/__init__.py
@@ -2,5 +2,5 @@
 #import awesome_streamlit.core as core
 #import awesome_streamlit.database as database
 #import awesome_streamlit.experiments as experiments
-#import awesome_streamlit.shared as shared
+import awesome_streamlit.shared as shared
 #import awesome_streamlit.testing as testing
