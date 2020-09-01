a66c79a7bc7e00acb62a54e553ae605284eca2a1
Danny Tobisch <danny.tobisch@gmail.com>
2020-09-01 17:14:17 +0200

    v5

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -32,13 +32,17 @@ def write():
 
     img_spotify = list(data_spot['img'][0:5])
     url_spotify = list(data_spot['url'][0:5])
+    title_spotify = list(data_spot['episode_name'][0:5])
 
     img_yt = list(data_yt['images'][0:5])
     url_yt = list(data_yt['url'][0:5])
+    title_yt = list(data_yt['name'][0:5])
 
     url_medium = list(data_medium['url'][0:5])
+    title_medium = list(data_medium['title'][0:5])
 
     url_arxiv = list(data_arxiv['url'][0:5])
+    title_arxiv = list(data_arxiv['title'][0:5])
 
     st.components.v1.html(f'<div class="row">\
                       <div class="column">\
@@ -47,39 +51,39 @@ def write():
                       style="vertical-align:middle;margin:3px 3px"></a>\
                       <a target="_blank" rel="noopener noreferrer"\
                       href={url_spotify[0]}><img src={img_spotify[0]} width="100" height="100"\
-                      style="vertical-align:middle;margin:3px 3px;border-radius:10%""></a>\
+                      title="{title_spotify[0]}" style="vertical-align:middle;margin:3px 3px;border-radius:10%""></a>\
                       <a target="_blank" rel="noopener noreferrer"\
                        href={url_spotify[1]}><img src={img_spotify[1]} width="100" height="100"\
-                      style="vertical-align:middle;margin:3px 3px;padding-right:20px;border-radius:10%""></a>\
+                      title="{title_spotify[1]}" style="vertical-align:middle;margin:3px 3px;padding-right:20px;border-radius:10%""></a>\
                       <a target="_blank" rel="noopener noreferrer"\
                         href="https://youtube.com"><img src="https://stagewp.sharethis.com/wp-content/uploads/2018/02/youtube.png" width="100" height="100"\
                       style="vertical-align:middle;margin:3px 3px;padding-left:2px;border-radius:10%"></a>\
                       <a target="_blank" rel="noopener noreferrer"\
                        href={url_yt[0]}><img src={img_yt[0]} width="100" height="100"\
-                      style="vertical-align:middle;margin:3px 3px;border-radius:10%"></a>\
+                      title="{title_yt[0]}" style="vertical-align:middle;margin:3px 3px;border-radius:10%"></a>\
                       <a target="_blank" rel="noopener noreferrer"\
-                       href={url_yt[1]}><img src={img_yt[1]} width="100" height="100"\
+                       title="{title_yt[1]}" href={url_yt[1]}><img src={img_yt[1]} width="100" height="100"\
                       style="vertical-align:middle;margin:3px 3px;border-radius:10%"></a>\
                       </div>\
                       <div class="column">\
                       <a target="_blank" rel="noopener noreferrer"\
                        href={url_spotify[2]}><img src={img_spotify[2]} width="100" height="100"\
-                      style="vertical-align:middle;margin:3px 3px;border-radius:10%"></a>\
+                      title="{title_spotify[2]}" style="vertical-align:middle;margin:3px 3px;border-radius:10%"></a>\
                       <a target="_blank" rel="noopener noreferrer"\
                        href={url_spotify[3]}><img src={img_spotify[3]} width="100" height="100"\
-                      style="vertical-align:middle;margin:3px 3px;border-radius:10%"></a>\
+                      title="{title_spotify[3]}" style="vertical-align:middle;margin:3px 3px;border-radius:10%"></a>\
                       <a target="_blank" rel="noopener noreferrer"\
                        href={url_spotify[4]}><img src={img_spotify[4]} width="100" height="100"\
-                      style="vertical-align:middle;margin:3px 3px;padding-right:20px;border-radius:10%"></a>\
+                      title="{title_spotify[4]}" style="vertical-align:middle;margin:3px 3px;padding-right:20px;border-radius:10%"></a>\
                       <a target="_blank" rel="noopener noreferrer"\
                        href={url_yt[2]}><img src={img_yt[2]} width="100" height="100"\
-                      style="vertical-align:middle;margin:3px 3px;padding-left:2px;border-radius:10%"></a>\
+                      title="{title_yt[2]}" style="vertical-align:middle;margin:3px 3px;padding-left:2px;border-radius:10%"></a>\
                      <a target="_blank" rel="noopener noreferrer"\
                       href={url_yt[3]}><img src={img_yt[3]} width="100" height="100"\
-                      style="vertical-align:middle;margin:3px 3px;border-radius:10%"></a>\
+                      title="{title_yt[3]}" style="vertical-align:middle;margin:3px 3px;border-radius:10%"></a>\
                       <a target="_blank" rel="noopener noreferrer"\
                        href={url_yt[4]}><img src={img_yt[4]} width="100" height="100"\
-                      style="vertical-align:middle;margin:3px 3px;border-radius:10%"></a>\
+                      title="{title_yt[4]}" style="vertical-align:middle;margin:3px 3px;border-radius:10%"></a>\
                       </div>\
                       </div>', height=250)
 
@@ -93,16 +97,16 @@ def write():
                       style="vertical-align:middle;margin:3px 3px;border-radius:10%"></a>\
                       <a target="_blank" rel="noopener noreferrer"\
                       href={url_medium[1]}><img src="https://miro.medium.com/fit/c/256/256/1*6_fgYnisCa9V21mymySIvA.png"  width="100" height="100"\
-                      style="vertical-align:middle;margin:3px 3px;padding-right:20px;border-radius:10%"></a>\
+                      title="test" style="vertical-align:middle;margin:3px 3px;padding-right:20px;border-radius:10%"></a>\
                       <a target="_blank" rel="noopener noreferrer"\
                        href="https://arxiv.org"><img src="https://pbs.twimg.com/profile_images/958432197987401728/QLeEVLC_.jpg" width="100" height="100"\
                       style="vertical-align:middle;margin:3px 3px;border-radius:50%;padding-left:2px"></a>\
                       <a target="_blank" rel="noopener noreferrer"\
                        href={url_arxiv[0]}><img src="https://pbs.twimg.com/profile_images/958432197987401728/QLeEVLC_.jpg" width="100" height="100"\
-                      style="vertical-align:middle;margin:3px 3px;border-radius:10%"></a>\
+                      title="{title_arxiv[0]}"" style="vertical-align:middle;margin:3px 3px;border-radius:10%"></a>\
                       <a target="_blank" rel="noopener noreferrer"\
                        href={url_arxiv[1]}><img src="https://pbs.twimg.com/profile_images/958432197987401728/QLeEVLC_.jpg" width="100" height="100"\
-                      style="vertical-align:middle;margin:3px 3px;border-radius:10%"></a>\
+                      title="{title_arxiv[1]}"" style="vertical-align:middle;margin:3px 3px;border-radius:10%"></a>\
                       </div>\
                       </div>', height=250)
 
